7414787f46b45614d9ad0af58c0c63367b5ff9fc
Josh Martinez <37962558+jm4rtinez@users.noreply.github.com>
2023-06-06 17:47:29 -0600

    Add docs for `Builder`

--- src/builder.ts
+++ src/builder.ts
@@ -15,22 +15,75 @@
  */
 
 /**
+ * Construction with the Builder pattern.
  *
+ * @remarks
+ *
+ * `Builder<T, TFinish>` provides an interface for constructing values. A
+ * `Builder<T, TFinish>` accepts values of type `T` and produces a result of
+ * type `TFinish`.
+ *
+ * `Builder` objects are often used to construct various containers, but they
+ * can be useful in any scenario that involves aggregating or combining values
+ * to produce a final result.
+ *
+ * ## Importing from this module
+ *
+ * The `Builder` interface can be imported as named:
+ *
+ * ```ts
+ * import type { Builder } from "@neotype/prelude/builder.js";
+ * ```
+ *
+ * ## Implementing `Builder`
+ *
+ * Implementors of `Builder` must implement the `add` and `finish` methods.
+ *
+ * -   `add` accepts a value to combine with any existing values in the
+ *     `Builder`.
+ * -   `finish` returns the final result from the `Builder`. Implementors should
+ *     specify the behavior of the `Builder` after `finish` is called, and
+ *     whether any further sequences of method calls are allowed.
+ *
+ * @example Creating a `Builder` for arrays
+ *
+ * Consider an `ArrayBuilder` class that builds an array by appending values:
+ *
+ * ```ts
+ * import type { Builder } from "@neotype/prelude/builder.js";
+ *
+ * class ArrayBuilder<in out T> implements Builder<T, T[]> {
+ *     readonly #elems: T[] = [];
+ *
+ *     add(elem: T): void {
+ *         this.#elems.push(elem);
+ *     }
+ *
+ *     finish(): T[] {
+ *         return this.#elems;
+ *     }
+ * }
+ * ```
  *
  * @module
  */
 
 /**
- *
+ * An interface that produces a result from added values.
  */
 export interface Builder<in T, out TFinish> {
 	/**
-	 *
+	 * Add a single element to this `Builder`.
 	 */
 	add(val: T): void;
 
 	/**
+	 * Return the result of all elements added to this `Builder`.
 	 *
+	 * @remarks
+	 *
+	 * Implementors of `Builder` should define their behavior after `finish` is
+	 * called, and whether any further sequences of method calls are allowed.
 	 */
 	finish(): TFinish;
 }
